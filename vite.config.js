479f43cf847693860c9aa56b3ea4f8abf2e04a24
gitzak <zakaria.belrhali@gmail.com>
2024-11-06 22:42:45 +0100

    modif

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-})
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    const proxy_url =
+        process.env.VITE_DEV_REMOTE === "remote"
+            ? process.env.VITE_BACKEND_SERVER
+            : "http://localhost:3000/";
+
+    const config = {
+        plugins: [react()],
+        build: {
+            outDir: "dist",
+            rollupOptions: {
+                output: {
+                    manualChunks: undefined,
+                },
+            },
+        },
+        resolve: {
+            base: "/",
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+            },
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: proxy_url,
+                    // changeOrigin: true,
+                    // secure: false,
+                },
+            },
+        },
+    };
+    return defineConfig(config);
+};
